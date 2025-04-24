c91717e6d3ccbc3ff2ae13e194237ede157fd4b2
alubtsova <alina22yakupova@gmail.com>
2025-04-25 01:19:32 +0500

    Add MANIFEST.MF

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.print("Р’РІРµРґРёС‚Рµ С‚РµРєСЃС‚ Рё РЅР°Р¶РјРёС‚Рµ <Enter>: ");
+        String text = new Scanner(System.in).nextLine();
+        System.out.println("Р”Р»РёРЅР° С‚РµРєСЃС‚Р°: " + text.length());
     }
 }
